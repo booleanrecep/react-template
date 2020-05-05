692aa50d125bb9fe0c5bc0fe5abccd0e75186912
Recep Öztürk <turkishteacher23@gmail.com>
2020-05-05 08:41:07 +0300

    Files are ready to use

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h3>Hello World </h3>
+  </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path=require("path")
+const HTMLWebpackPlugiin=require("html-webpack-plugin")
+
+module.exports={
+    entry:"./src/index.js",
+    output:{
+        path:path.join(__dirname,"build"),
+        filename:"bundle.js"
+    },
+    module:{
+        rules:[
+        {
+            test:/\.(js|jsx)$/,
+            exclude:/node_modules/,
+            use:{
+                loader:"babel-loader"
+            }
+        },
+        {
+            test:/\.html$/,
+            use:[
+                {
+                    loader:"html-loader"
+                }
+            ]
+        },
+        {
+            test:/\.css$/,
+            use:["style-loader","css-loader"]
+        }
+    ],
+    },
+    
+    plugins:[
+        new HTMLWebpackPlugiin({
+            template:"./public/index.html",
+            filename:"./index.html"
+        })
+    ],
+    devtool:"cheap-module-eval-source-map",
+    devServer:{
+        contentBase:path.join(__dirname,"public")
+    }
+
+    
+}
